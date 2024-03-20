8b86c752a322386888be1509fcc6183ca394d5b6
dani10mv <danielmirallesv@gmail.com>
2024-03-20 14:42:32 +0100

    Added color picker

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import {ColorSelector} from '@/components/ColorSelector'
 import {Palette} from '@/components/Palette'
+import {getColorInfo} from '@/services/colors'
 function App() {
 
-
+  const[mainColor,setMainColor] =useState("#C0E294")
   return (
     <main>
       <h1>THE COOL PALETTE.</h1>
-      <ColorSelector/>
+      <ColorSelector mainColor={mainColor} setMainColor={setMainColor}/>
       <Palette/>
     </main> 
   )
--- src/components/ColorSelector/ColorSelector.jsx
+++ src/components/ColorSelector/ColorSelector.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import "./ColorSelector.css";
-export default function ColorSelector() {
+export default function ColorSelector({mainColor,setMainColor}) {
+
+  const updateColor = (event) => {
+      setMainColor(event.target.value);
+      console.log("Searching...", event.target.value);
+      // your API call logic goes here
+  }
+  
+  const debounce = (callback, waitTime) => {
+      let timer;
+      return (...args) => {
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+              callback(...args);
+          }, waitTime);
+      };
+  }
+  
+  const debounceHandler = debounce(updateColor, 300);
+
+
   return (
     <div className="color-selector">
       <div className="color-info">
         <span className="color-name">Green</span>
-        <span className="color-hex">#C0E294</span>
+        <span className="color-hex">{mainColor}</span>
       </div>
-      <div className="color-preview"></div>
+      <input
+        type="color"
+        className="color-preview"
+        defaultValue={mainColor}
+        onChange={debounceHandler}
+      ></input>
     </div>
   );
 }
