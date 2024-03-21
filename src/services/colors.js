cc5683e8c8552b20f52dd836931d0de765ca12c6
dani10mv <danielmirallesv@gmail.com>
2024-03-21 17:04:05 +0100

    removes "#" on services urls

--- src/services/colors.js
+++ src/services/colors.js
@@ -2,5 +2,5 @@ import asyncFetch from "@/utils/asyncFetch";
 const COLORS_API_URL = "https://www.thecolorapi.com/id?hex=";
 
 export const getColorInfo = async (colorHex) => {
-  return await asyncFetch(COLORS_API_URL + colorHex);
+  return await asyncFetch(COLORS_API_URL + colorHex.replace("#",""));
 };
--- src/services/palette.js
+++ src/services/palette.js
@@ -2,5 +2,5 @@ import asyncFetch from "@/utils/asyncFetch";
 const PALETTE_API_URL = "http://palett.es/API/v1/palette/from/";
 
 export const getPalette = async (colorHex) => {
-  return await asyncFetch(PALETTE_API_URL + colorHex);
+  return await asyncFetch(PALETTE_API_URL + colorHex.replace("#",""));
 };
