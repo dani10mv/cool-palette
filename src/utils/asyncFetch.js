23d1d0676d8cd90f4b3edce52d920fcfd3a066ae
dani10mv <danielmirallesv@gmail.com>
2024-03-21 17:04:37 +0100

    wip: uses in dev cors anywhere prefix

--- src/utils/asyncFetch.js
+++ src/utils/asyncFetch.js
@@ -1,6 +1,11 @@
+const PREFIX = "https://cors-anywhere.herokuapp.com/"
 export default async function asyncFetch(url){
-  const response =await  fetch(url)
+  const response =await  fetch(PREFIX+url, {
+		method: 'GET',
+		headers: {
+			'origin': 'your-rapidapi-key',
+		},
+	})
   const data = await response.json()
-  console.log(data)
   return data
 }
