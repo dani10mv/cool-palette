c3aad969bbeba311851aab4ca9e0036e7c55c6fc
dani10mv <danielmirallesv@gmail.com>
2024-03-21 17:03:02 +0100

    gets palette info on color change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
-import { useState } from 'react'
-import './App.css'
-import {ColorSelector} from '@/components/ColorSelector'
-import {Palette} from '@/components/Palette'
-import {getColorInfo} from '@/services/colors'
-function App() {
+import { useState, useEffect } from "react";
+import "./App.css";
+import { ColorSelector } from "@/components/ColorSelector";
+import { Palette } from "@/components/Palette";
+import { getColorInfo } from "@/services/colors";
+import { getPalette } from "@/services/palette";
 
-  const[mainColor,setMainColor] =useState("#C0E294")
+function App() {
+  const [mainColor, setMainColor] = useState("#C0E294");
+  const [mainColorName, setMainColorName] = useState("Yellow Green");
+
+  const [palette, setPalette] = useState(null);
+
+  const getAllPaletteInfo = async (paletteColors) => {
+    return await Promise.all(
+      paletteColors.map((colorHex) => getColorInfo(colorHex))
+    );
+  };
+
+  const handleColorChange = async () => {
+    const paletteColors = await getPalette(mainColor);
+    const paletteInfo = await getAllPaletteInfo(paletteColors);
+    setMainColorName(
+      paletteInfo.find((colorInfo) => colorInfo.hex.value === mainColor).name
+        .value
+    );
+    setPalette(paletteInfo);
+  };
+
+  useEffect(() => {
+    handleColorChange();
+    return;
+  }, [mainColor]);
+
   return (
     <main>
       <h1>THE COOL PALETTE.</h1>
-      <ColorSelector mainColor={mainColor} setMainColor={setMainColor}/>
-      <Palette/>
-    </main> 
-  )
+      <ColorSelector
+        colorName={mainColorName}
+        mainColor={mainColor}
+        setMainColor={setMainColor}
+      />
+      <Palette paletteInfo={palette} />
+    </main>
+  );
 }
 
-export default App
+export default App;
