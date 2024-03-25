a45575d20e27c17f4976e099fad010f87bed88e1
dani10mv <danielmirallesv@gmail.com>
2024-03-25 13:37:31 +0100

    using palette store to manage state

--- src/components/ColorSelector/ColorSelector.jsx
+++ src/components/ColorSelector/ColorSelector.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ColorSelector.css";
+import { usePalette } from "@/store/palette";
 
 const debounce = (callback, waitTime) => {
-    let timer;
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            callback(...args);
-        }, waitTime);
-    };
-}
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+    }, waitTime);
+  };
+};
 
-export default function ColorSelector({colorName="",mainColor,setMainColor}) {
+export default function ColorSelector({ colorName = "Yellow Green" }) {
+  const { updateWithColor, mainColor, mainColorName } = usePalette();
 
   const updateColor = (event) => {
-      setMainColor(event.target.value.toUpperCase());
-  }
-  const debounceHandler = debounce(updateColor, 300); 
+    const colorHex = event.target.value.toUpperCase();
+    updateWithColor(colorHex);
+  };
+  const debounceHandler = debounce(updateColor, 300);
 
+  useEffect(() => {
+    updateWithColor(mainColor);
+  }, []);
 
   return (
     <div className="color-selector">
       <div className="color-info">
-        <span className="color-name">{colorName}</span>
+        <span className="color-name">{mainColorName || colorName}</span>
         <span className="color-hex">{mainColor}</span>
       </div>
       <input
--- src/components/Palette/Palette.jsx
+++ src/components/Palette/Palette.jsx
@@ -1,28 +1,63 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { useEffect } from "react";
 import "./Palette.css";
+import { usePalette } from "@/store/palette";
 
-export default function Palette({ paletteInfo }) {
-  const PaletteColor = ({ colorHex, colorName, contrast }) => {
+export default function Palette() {
+  const PaletteColor = ({ index }) => {
+    const divRef = useRef();
+    const [color, setColor] = useState(null);
+      
+      const cambiarColor = (color) => {
+        const paletteColor = divRef.current;
+        
+        paletteColor.animate(
+          {
+            backgroundColor: color.hex,
+        },
+        {
+          delay: index * 0.2 * 1000,
+          duration: 4000,
+          fill: "forwards",
+          easing: "ease",
+        }
+        );
+        setColor(color)
+      };
+      const subs = usePalette.subscribe((state) => cambiarColor(state.palette[index]));
+
     return (
-      <div className="palette-color" style={{backgroundColor:colorHex}}>
-        <span className="color-name" style={{ color: contrast }}>
-          {colorName}
-        </span>
-        <span className="color-hex" style={{ color: contrast, opacity:'0.5' }}>{colorHex}</span>
+      <div ref={divRef} className="palette-color">
+        {color && (
+          <>
+            <span className="color-name" style={{ color: color.contrast }}>
+              {color.name}
+            </span>
+            <span
+              className="color-hex"
+              style={{ color: color.contrast, opacity: "0.5" }}
+            >
+              {color.hex}
+            </span>
+          </>
+        )}
       </div>
     );
   };
 
-  return (
-    <div className="palette">
-      {paletteInfo &&
-        paletteInfo.map(({ hex, name, contrast }) => (
-          <PaletteColor
-            colorHex={hex.value}
-            colorName={name.value}
-            contrast={contrast.value}
-          />
-        ))}
-    </div>
-  );
+  const createColors = () => {
+    let listOfColors = [];
+    console.log("creo colores");
+    for (let i = 0; i < 5; i++) {
+      listOfColors.push(
+        <PaletteColor
+          key={i}
+          index={i}
+        />
+      );
+    }
+    return listOfColors;
+  };
+
+  return <div className="palette">{createColors()}</div>;
 }
